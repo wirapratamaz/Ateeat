37da765ab87d0a9d9874645c102f47b004ebd6fa
wirapratamaz <wirabagus185@gmail.com>
2023-02-01 20:35:25 +0800

    add seller user model

--- user/admin.py
+++ user/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
-from .models import BuyerUser
+from .models import BuyerUser, SellerUser
 
 # Register your models here.
 #models BuyerUser
 class BuyerUserAdmin(admin.ModelAdmin):
     list_display = ['authority', 'name', 'email', 'phone_number', 'address', 'latitude', 'longitude']
-    
+
+class SellerUserAdmin(admin.ModelAdmin):
+    list_display = ['authority', 'name', 'email', 'phone_number', 'address', 'latitude', 'longitude']
+
 admin.site.register(BuyerUser, BuyerUserAdmin)
--- user/models.py
+++ user/models.py
@@ -9,3 +9,15 @@ class BuyerUser(models.Model):
     address = models.CharField(max_length=150)
     latitude = models.FloatField()
     longitude = models.FloatField()
+    
+class SellerUser(models.Model):
+    authority = models.CharField(max_length=100)
+    name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=320)
+    phone_number = models.CharField(max_length=15)
+    address = models.CharField(max_length=150)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    
+    def __str__(self):
+        return self.authority
